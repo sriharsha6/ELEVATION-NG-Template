aa920b1d42a835168acb2af15799acb8cefb49cd
sai1004 <saikiran.bavandla@gmail.com>
2019-11-09 13:09:29 +0530

    commit init

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,96 @@
+import { NgModule } from "@angular/core";
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatGridListModule,
+  MatSidenavModule,
+  MatSortModule,
+  MatTableModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatRadioModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatChipsModule,
+  MatTooltipModule,
+  MatExpansionModule,
+  MatDialogModule,
+  MatAutocompleteModule,
+  MatTabsModule,
+  MatSlideToggleModule,
+  MatPaginatorModule,
+  MatNativeDateModule,
+  MatDividerModule
+} from "@angular/material";
+  
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    MatNativeDateModule,
+    MatDividerModule
+  ],
+  
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    MatNativeDateModule,
+    MatDividerModule
+  ]
+})
+
+
+export class MaterialModule {}
